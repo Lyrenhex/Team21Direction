51f139dba527ceccf606e407e12615dfec4e20d3
Damian Heaton <dh64784@gmail.com>
2022-01-18 15:11:26 +0000

    Initial project setup

---
+++ PirateGame/desktop/src/com/team21direction/pirategame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.team21direction.pirategame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.team21direction.pirategame.PirateGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PirateGame(), config);
+	}
+}
