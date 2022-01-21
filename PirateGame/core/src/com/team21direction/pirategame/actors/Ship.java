0835927e5c2f1a8c2a6d36f970f3770e1a5b33ed
Damian Heaton <dh64784@gmail.com>
2022-01-21 12:45:05 +0000

    .Load directional textures for Ships.

--- PirateGame/core/src/com/team21direction/pirategame/actors/Ship.java
+++ PirateGame/core/src/com/team21direction/pirategame/actors/Ship.java
@@ -5,12 +5,17 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
+import java.util.HashMap;
+
 public class Ship extends Actor {
+    public enum Direction{Up, UpLeft, UpRight, Left, Right, Down, DownLeft, DownRight}
+    private Direction direction = Direction.Right;
+
     private int health = 100;
     public static int damage = 10;
     public College parentCollege; // The College this ship is allied with.
 
-    private final Texture ship;
+    private final HashMap<Direction, Texture> textures;
 
     /**
      * Construct a new Ship which is a member of the supplied parentCollege.
@@ -18,10 +23,26 @@ public class Ship extends Actor {
      */
     public Ship(College parentCollege) {
         this.parentCollege = parentCollege;
-        if (parentCollege == null)
-            ship = new Texture(Gdx.files.internal("ship.png"));
-        else
-            ship = new Texture(Gdx.files.internal("ship" + parentCollege.getCollegeName() + ".png"));
+        textures = new HashMap<>();
+        if (parentCollege == null) {
+            textures.put(Direction.Up, new Texture(Gdx.files.internal("ship-up.png")));
+            textures.put(Direction.UpLeft, new Texture(Gdx.files.internal("ship-upleft.png")));
+            textures.put(Direction.UpRight, new Texture(Gdx.files.internal("ship-upright.png")));
+            textures.put(Direction.Left, new Texture(Gdx.files.internal("ship-left.png")));
+            textures.put(Direction.Right, new Texture(Gdx.files.internal("ship-right.png")));
+            textures.put(Direction.Down, new Texture(Gdx.files.internal("ship-down.png")));
+            textures.put(Direction.DownLeft, new Texture(Gdx.files.internal("ship-downleft.png")));
+            textures.put(Direction.DownRight, new Texture(Gdx.files.internal("ship-downright.png")));
+        } else {
+            textures.put(Direction.Up, new Texture(Gdx.files.internal(parentCollege.getCollegeName() + "ship-up.png")));
+            textures.put(Direction.UpLeft, new Texture(Gdx.files.internal(parentCollege.getCollegeName() + "ship-upleft.png")));
+            textures.put(Direction.UpRight, new Texture(Gdx.files.internal(parentCollege.getCollegeName() + "ship-upright.png")));
+            textures.put(Direction.Left, new Texture(Gdx.files.internal(parentCollege.getCollegeName() + "ship-left.png")));
+            textures.put(Direction.Right, new Texture(Gdx.files.internal(parentCollege.getCollegeName() + "ship-right.png")));
+            textures.put(Direction.Down, new Texture(Gdx.files.internal(parentCollege.getCollegeName() + "ship-down.png")));
+            textures.put(Direction.DownLeft, new Texture(Gdx.files.internal(parentCollege.getCollegeName() + "ship-downleft.png")));
+            textures.put(Direction.DownRight, new Texture(Gdx.files.internal(parentCollege.getCollegeName() + "ship-downright.png")));
+        }
     }
 
     public Ship() {
@@ -37,7 +58,11 @@ public class Ship extends Actor {
         return damage;
     }
 
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+
     public void draw(SpriteBatch batch, float parentAlpha) {
-        batch.draw(ship, 0, 0);
+        batch.draw(textures.get(direction), 0, 0);
     }
 }
