7287effefae4f4c20a70f430fb4965e231020025
zain723 <zain.n.alshaikh@gmail.com>
2022-01-23 15:41:18 +0000

    Create MainScreen.java

---
+++ PirateGame/core/src/com/team21direction/pirategame/screens/MainScreen.java
@@ -0,0 +1,72 @@
+package com.team21direction.pirategame.screens;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.team21direction.pirategame.PirateGame;
+
+public class MainScreen implements Screen {
+
+    private Sprite sprite;
+    private SpriteBatch batch;
+    private Texture texture;
+
+    @Override
+    public void show() {
+       
+    
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void render(float delta) {
+        // TODO Auto-generated method stub
+        batch.begin();
+        batch.draw(texture, 50, 50);
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void pause() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void resume() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void hide() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void dispose() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
