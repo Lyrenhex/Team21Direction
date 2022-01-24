db8a62ae687325deed4485d957c952cf8574e657
Damian Heaton <dh64784@gmail.com>
2022-01-24 13:29:32 +0000

    Fix `MainScreen` class definition

--- PirateGame/core/src/com/team21direction/pirategame/screens/MainScreen.java
+++ PirateGame/core/src/com/team21direction/pirategame/screens/MainScreen.java
@@ -3,6 +3,8 @@ package com.team21direction.pirategame.screens;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
@@ -23,17 +25,41 @@ public class MainScreen implements Screen {
     private SpriteBatch batch;
     private Texture texture;
 
+    private PirateGame game;
+    protected Stage stage;
+    private Viewport viewport;
+    protected Skin skin;
+    protected TextureAtlas atlas;
+
+    private OrthographicCamera camera;
+
+    public MainScreen(PirateGame game) {
+        this.game = game;
+        skin = new Skin(Gdx.files.internal("uiskin.json"));
+        atlas = new TextureAtlas(Gdx.files.internal("uiskin.atlas"));
+
+        camera = new OrthographicCamera();
+
+        batch = new SpriteBatch();
+
+        viewport = new FitViewport(game.WORLD_WIDTH, game.WORLD_HEIGHT, camera);
+        viewport.apply();
+
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
+        camera.update();
+
+        stage = new Stage(viewport, batch);
+    }
+
     @Override
     public void show() {
-       
-    
         // TODO Auto-generated method stub
         
     }
 
     @Override
     public void render(float delta) {
-        // TODO Auto-generated method stub
+        ScreenUtils.clear(0, 0.6f, 1, 1);
         batch.begin();
         batch.draw(texture, 50, 50);
 
@@ -41,8 +67,9 @@ public class MainScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-        // TODO Auto-generated method stub
-        
+        viewport.update(width, height);
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
+        camera.update();
     }
 
     @Override
@@ -65,8 +92,9 @@ public class MainScreen implements Screen {
 
     @Override
     public void dispose() {
-        // TODO Auto-generated method stub
-        
+        batch.dispose();
+        skin.dispose();
+        atlas.dispose();
     }
     
 }
