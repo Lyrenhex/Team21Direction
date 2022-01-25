a262ac0673928f492759986b84c72bca537582ce
tomstafijs <96183423+tomstafijs@users.noreply.github.com>
2022-01-25 11:50:24 +0000

    Update PirateGame.java

--- PirateGame/core/src/com/team21direction/pirategame/PirateGame.java
+++ PirateGame/core/src/com/team21direction/pirategame/PirateGame.java
@@ -2,6 +2,11 @@ package com.team21direction.pirategame;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.team21direction.pirategame.screens.MainScreen;
 import com.team21direction.pirategame.screens.TitleScreen;
 
@@ -14,17 +19,34 @@ public class PirateGame extends Game implements ApplicationListener {
 	/* Screens */
 	public TitleScreen titleScreen;
 	public MainScreen mainScreen;
-
+	SpriteBatch batch;
+    Texture img;
+    OrthographicCamera camera;
+	
 	@Override
 	public void create () {
 		titleScreen = new TitleScreen(this);
 		mainScreen = new MainScreen(this);
+		batch = new SpriteBatch();
+        img = new Texture("rsH6n.jpg");
+        camera = new OrthographicCamera(1280, 720);
+        camera.update();
 
 		this.setScreen(titleScreen);
 	}
 
 	@Override
 	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+
+        batch.setProjectionMatrix(camera.combined);
+
+        batch.begin();
+        batch.draw(img, 0, 0);
+        batch.end();
 		super.render();
 	}
 
@@ -47,4 +69,12 @@ public class PirateGame extends Game implements ApplicationListener {
 	public void dispose () {
 		super.dispose();
 	}
+	
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+    float x = Gdx.input.getDeltaX();
+    float y = Gdx.input.getDeltaY();
+
+    camera.translate(-x,y);
+    return true;
+}
 }
