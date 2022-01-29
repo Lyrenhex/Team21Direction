a8967f47d48f00565015f03bc3bb907449cfa632
Damian Heaton <dh64784@gmail.com>
2022-01-29 18:20:22 +0000

    Fix poor performance during White Flag Routine.

--- PirateGame/core/src/com/team21direction/pirategame/actions/WhiteFlagRoutine.java
+++ PirateGame/core/src/com/team21direction/pirategame/actions/WhiteFlagRoutine.java
@@ -17,11 +17,11 @@ public class WhiteFlagRoutine extends Action {
     public boolean act(float delta) {
         College college = (College)actor;
         totalAnimationDuration += delta;
-        animState = (int)(totalAnimationDuration / 0.5f);
-        if (animState > 3) {
-            animState = 3;
+        int newAnimState = (int)(totalAnimationDuration / 0.5f);
+        if (animState != newAnimState && newAnimState <= 3) {
+            animState = newAnimState;
+            college.collegeBase = new Sprite(new Texture(Gdx.files.internal("colleges/college-defeated-" + animState + ".png")));
         }
-        college.collegeBase = new Sprite(new Texture(Gdx.files.internal("colleges/college-defeated-" + animState + ".png")));
         if (totalAnimationDuration >= 10.0f) {
             // mercy; the College joins the player's College.
             college.setCollegeName(college.screen.player.parentCollege.getCollegeName());
