7f667ce9047ae18498641407a0ad6a17e3ad931f
Damian Heaton <dh64784@gmail.com>
2022-01-29 18:48:06 +0000

    Set window title

--- PirateGame/desktop/src/com/team21direction/pirategame/desktop/DesktopLauncher.java
+++ PirateGame/desktop/src/com/team21direction/pirategame/desktop/DesktopLauncher.java
@@ -7,6 +7,7 @@ import com.team21direction.pirategame.PirateGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle(PirateGame.TITLE);
 		config.setForegroundFPS(60);
 		new Lwjgl3Application(new PirateGame(), config);
 	}
